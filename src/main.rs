0d3c853eac691d5fbef8b2b8f115db6beb334e8e
Luan Almeida <luanlmd@gmail.com>
2023-09-29 17:40:13 -0300

    infinite threads server

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use std::env;
-//use serde::{Serialize};
+use std::thread;
+use std::time::Duration;
 use serde_json;
 use std::net::TcpListener;
 use std::net::TcpStream;
@@ -27,7 +28,9 @@ fn main() {
 
     for stream in listener.incoming() {
         let stream = stream.unwrap();
-        handle_connection(stream);
+        thread::spawn(|| {
+            handle_connection(stream);
+        });
     }
 }
 
@@ -35,9 +38,9 @@ fn handle_connection(mut stream: TcpStream) {
     let mut buffer = [0; 1024];
 
     stream.read(&mut buffer).unwrap();
-    // println!("Request: {}", String::from_utf8_lossy(&buffer[..]));
 
     let get = b"GET / HTTP/1.1\r\n";
+    let sleep = b"GET /sleep HTTP/1.1\r\n";
 
     let (status_code, response_data) =
         if buffer.starts_with(get) {
@@ -46,6 +49,15 @@ fn handle_connection(mut stream: TcpStream) {
                 Response {
                     success: true,
                     message: "Hello from Rust".to_string()
+                }
+            )
+        } else if buffer.starts_with(sleep) {
+            thread::sleep(Duration::from_secs(5));
+            (
+                StatusCode::Ok,
+                Response {
+                    success: true,
+                    message: "Sleep from Rust".to_string()
                 }
             )
         } else {
